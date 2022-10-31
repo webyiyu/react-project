0f9463f7f512e2286463ef95dae352025b3917b0
hySmart <906325802@qq.com>
2022-10-31 23:28:59 +0800

    feat: react新的生命周期

--- basic/src/component.js
+++ basic/src/component.js
@@ -2,7 +2,7 @@
  * @Author: hySmart 906325802@qq.com
  * @Date: 2022-10-19 23:13:39
  * @LastEditors: hySmart 906325802@qq.com
- * @LastEditTime: 2022-10-25 23:29:47
+ * @LastEditTime: 2022-10-31 23:27:18
  * @FilePath: \react-project\basic\src\component.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -90,11 +90,18 @@ export class Component {
   forceUpdate() {
     let oldRenderVDom = this.oldRenderVDom
     let oldDOM = findDOM(oldRenderVDom)
+    if(this.constructor.getDerivedStateFromProps) {
+      let newState = this.constructor.getDerivedStateFromProps(this.props, this.state)
+      if(newState){
+        this.state = {...this.state, ...newState}
+      }
+    }
+    const snapShot = this.getSnapshotBeforeUpdate && this.getSnapshotBeforeUpdate()
     let newRenderVDom = this.render()
     compareTwoVDom(oldDOM.parentNode, oldRenderVDom, newRenderVDom)
     this.oldRenderVDom = newRenderVDom
     if(this.componentDidUpdate) {
-      this.componentDidUpdate(this.props, this.state)
+      this.componentDidUpdate(this.props, this.state, snapShot)
     }
   }
 }
